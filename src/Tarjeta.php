4eacc17e40526c3c38c854707e08ebf4ed9d94be
Juanchoox <32881812+Juanchoox@users.noreply.github.com>
2017-10-24 17:20:25 -0300

    #Commit Nº1
    Se escribió la función sacarBici, con el cobro de boleto una vez al día.

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -5,7 +5,9 @@ namespace TpFinal;
 class Tarjeta {
     
     protected saldo = 0;
-
+    protected horaBici = 0;
+    protected diaBici = 0;
+    
     public function saldo() {
         return $this->saldo;
     }
@@ -23,15 +25,40 @@ class Tarjeta {
             }
         }
     }
-
+    
+    public function sacarBici(Transporte $transporte) {
+        if($this->diaBici != $transporte->dia) {
+            $this->diaBici = 0;
+        }
+        if($this->diaBici = 0) {
+            if($this->saldo < 12.75) {
+                echo "No se puede retirar la bicicleta.";
+            }
+            else {
+                $this->saldo = $this->saldo - 12.75;
+                $this->horaBici = $transporte->hora;
+                $this->diaBici = $transporte->dia;
+                echo "Se ha retirado la bicicleta.";
+            }
+        }
+        else {
+            echo "Se ha retirado la bicicleta.";
+        }
+    }
 
 }
 
 class Transporte {
-    protected $tipoVehiculo;
+    
+    protected $lineaVehiculo
     protected $patente;
-    public function __construct($a, $b) {
-        $this->tipoVehiculo = $a;
+    protected $hora;
+    protected $dia;
+    
+    public function __construct($a, $b, $c, $d) {
+        $this->lineaVehiculo = $a;
         $this->patente = $b;
+        $this->hora = $c;
+        $this->dia = $d;
     }
 }
